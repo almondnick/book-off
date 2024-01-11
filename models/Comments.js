8c25bccb99b1f6c76ebf252ce6ce0a8c7fffae70
spadilla512 <spadilla512@gmail.com>
2024-01-10 20:48:58 -0600

    updated models file for index, user, and comments

---
+++ models/Comments.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require("../config/connection");
+
+class Comments extends Model {}
+
+Comments.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        comments_body: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        date_created: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "user",
+                key: "id",
+            },
+        },
+        reviews_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "reviews",
+                key: "id",
+            },
+        },
+       
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "comment",
+    }
+);
+module.exports = Comments;
