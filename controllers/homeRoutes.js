b92dd415eb5721066b4d4092e6dd644dac856814
Matt R <mattrallo2012@gmail.com>
2024-01-19 18:58:40 -0600

    routes that link the handibars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -16,4 +16,37 @@ router.get('/homepage', async (req, res) => {
         }); // login page which is our homepage
 });
 
+router.get('/mybooks', async (req, res) => {
+
+        res.render('mybooks', {
+                logged_in: req.session.logged_in 
+        }); // login page which is our homepage
+});
+
+
+router.get('/reviewpage', async (req, res) => {
+
+        res.render('reviewpage', {
+                logged_in: req.session.logged_in 
+        }); // login page which is our homepage
+});
+
+router.get('/searchpage', async (req, res) => {
+
+        res.render('searchpage', {
+                logged_in: req.session.logged_in 
+        }); // login page which is our homepage
+});
+
+router.get('/singlebook', async (req, res) => {
+
+        res.render('singlebook', {
+                logged_in: req.session.logged_in 
+        }); // login page which is our homepage
+});
+
+
+
+
+
 module.exports = router;
